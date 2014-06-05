bc6a966c7e4d3c7fb9cd4340614be9962ff47eda
Viraj Chitnis <chitnisviraj@gmail.com>
2014-06-04 23:42:28 -0400

    Added a api directory containing a basic api script

---
+++ api/index.php
@@ -0,0 +1,13 @@
+<?php
+/* API code goes here. The webpages will be using JavaScript to build 
+ * the interface after retrieving JSON from the server using this api.
+ * 
+ * Each possible data retrival and insertion operation will have its
+ * own 'if' clause that will be triggered based upon the 'GET' variables
+ * passed.
+ * 
+ * If no 'if' clause is triggered, the 'else' will be triggered which
+ * will display info about how to use the api. The 'else' will also
+ * check if invalid 'GET' variables have been set and will warn accordingly.
+*/
+?>
