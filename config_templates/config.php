fe1b0576d096cdece19ec477fe1b2fbec81cf4a6
Viraj Chitnis <chitnisviraj@gmail.com>
2014-06-05 10:47:59 -0400

    Added a config template
    The config template is meant to contain variables used by the program in various places. For use, it will be copied into the config directory during installation.

---
+++ config_templates/config.php
@@ -0,0 +1,5 @@
+<?php
+$database = "FamilyDB";			// Name of the database
+$username = "familydb";			// Username to access database by
+$password = "familydb_pass";	// Password to access database by
+?>
