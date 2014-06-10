d11bd504d6f508bceeefeadeebb4665654704665
Viraj Chitnis <chitnisviraj@gmail.com>
2014-06-10 08:48:00 -0400

    Basic header for the home page
    The home page now has a basic header that contains the username, settings page link and the logout link.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="stylesheets/design.css">
+		<title>Home - FamilyDB</title>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="header">
+				<h2 class="header_contents">User Name</h2>
+				<p class="header_contents">Settings</p>
+				<p class="header_contents">Logout</p>
+			</div>
+			<div class="push"></div>
+		</div>
+		<?php include("common/footer.php"); ?>
+	</body>
+</html>
