b66b852a72e219cb6794df6adb35c9fbdaff4c18
Viraj Chitnis <chitnisviraj@gmail.com>
2014-06-10 10:27:08 -0400

    Added trips.php
    Trips.php is the section of the database incharge of keeping track of and displaying all family trips for future visa purposes.

---
+++ trips.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="stylesheets/design.css">
+		<title>Trips - FamilyDB</title>
+	</head>
+	<body>
+		<div class="wrapper">
+			<?php include("common/header.php"); ?>
+			<div class="body_div">
+			</div>
+			<div class="push"></div>
+		</div>
+		<?php include("common/footer.php"); ?>
+	</body>
+</html>
